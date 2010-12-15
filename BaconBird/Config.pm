93b9dfd7bc798fb781636fd491322fc33c4cbcce
Andreas Krennmair <ak@synflood.at>
2010-12-15 10:36:45 +0100

    implemented configuration loading.

--- BaconBird/Config.pm
+++ BaconBird/Config.pm
@@ -9,8 +9,38 @@ has 'configfile' => (
 	default => $ENV{'HOME'} . "/.baconbird/config",
 );
 
+has 'configdata' => (
+	is => 'rw',
+	isa => 'HashRef',
+);
+
+has 'default_config' => (
+	is => 'rw',
+	isa => 'HashRef',
+);
+
+sub BUILD {
+	my $self = shift;
+	$self->default_config({
+		"browser"		=> "links %u",
+	});
+	$self->configdata({ });
+}
+
 sub load {
-	# TODO: implement
+	my $self = shift;
+	eval {
+		my $conf = new Config::General(-LowerCaseNames => 1, -ConfigFile => $self->configfile);
+		my %config = $conf->getall;
+		$self->configdata(\%config);
+		undef $conf;
+	};
+}
+
+sub get_value {
+	my $self = shift;
+	my ($key) = @_;
+	return $self->configdata->{$key} || $self->default_config->{$key};
 }
 
 no Moose;
