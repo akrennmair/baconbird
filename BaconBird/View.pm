eda3965d226fb49ac31062989384a469310f7698
Juan C. Muller <jcmuller@gmail.com>
2010-12-16 09:49:11 -0500

    Shorten short help for open url.

--- BaconBird/View.pm
+++ BaconBird/View.pm
@@ -26,7 +26,7 @@ use constant HELP_TIMELINE => [
 	{ key => BaconBird::KeyMap::KEY_DIRECT_MESSAGES, desc => "Direct Messages" },
 	{ key => BaconBird::KeyMap::KEY_SEARCH_RESULTS, desc => "Search Results" },
 	{ key => BaconBird::KeyMap::KEY_USER_TIMELINE, desc => "User Timeline" },
-	{ key => BaconBird::KeyMap::KEY_OPEN_URL, desc => "Open URLs in default browser" },
+	{ key => BaconBird::KeyMap::KEY_OPEN_URL, desc => "Open URLs" },
 ];
 
 use constant HELP_DM => [
